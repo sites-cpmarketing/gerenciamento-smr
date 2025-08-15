1ce3c4d114fcf418c443e9b949d3eb4096ecfa4e
Sites CP Marketing <sites.cpmarketing@gmail.com>
2025-08-15 13:55:02 +0000

    agora inclua por favor o que é necessário para que eu consiga instalar e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Montserrat:wght@900&display=swap" rel="stylesheet"/>
